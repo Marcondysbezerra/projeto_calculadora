8669381e406654ec4e26768026f059c892cbd8ef
Marcondys Bezerra <marcondysbezerra@gmail.com>
2022-01-25 19:32:12 -0300

    Implementado menu

---
+++ calculadora/menu.py
@@ -0,0 +1,9 @@
+def menu_opcoes():
+    print("======================")
+    print("+  s : Soma          +")
+    print("+  d : Divisão       +")
+    print("+  - : Subtração     +")
+    print("+  m : Multiplicação +")
+    print("======================")
+
+
